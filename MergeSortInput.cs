f437ee52dc49d1b7fa5bdc8ccbbfa9afc0ac1866
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-11-27 18:08:49 +0600

    Implement merge sort in C#

---
+++ MergeSortInput.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class MergeSortInput
+    {
+        public static void DoMergeSort(int[] numbers)
+        {
+            var sortestNumbers = MergeSort(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = sortestNumbers[i];
+            }
+        }
+        private static int[] MergeSort(int[] numbers)
+        {
+            if(numbers.Length <= 1)
+            {
+                return numbers;
+            }
+
+            var left = new List<int>();
+            var right = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 0)
+                    left.Add(numbers[i]);
+                else
+                    right.Add(numbers[i]);
+            }
+
+            left = MergeSort(left.ToArray()).ToList();
+            right = MergeSort(right.ToArray()).ToList();
+
+            return Merge(left, right);
+        }
+
+        private static int[] Merge(List<int> left, List<int> right)
+        {
+            var result = new List<int>();
+
+            while (ListNotEmpty(left) && ListNotEmpty(right))
+            {
+                if (left.First() <= right.First())
+                {
+                    MoveValueFromSourceToResult(left, result);
+                }
+                else
+                {
+                    MoveValueFromSourceToResult(right, result);
+                }
+            }
+            if (ListNotEmpty(left))
+            {
+                MoveValueFromSourceToResult(left, result);
+            }
+            else
+            {
+                MoveValueFromSourceToResult(right, result);
+            }
+            return result.ToArray();
+        }
+
+        private static bool ListNotEmpty(List<int> list)
+        {
+            return list.Count > 0;
+        }
+
+        private static void MoveValueFromSourceToResult(List<int> list, List<int> result)
+        {
+            result.Add(list.First());
+            list.RemoveAt(0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 22, 32, 11, 19, 9, 10, 22 };
+            MergeSortInput.DoMergeSort(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.Read();
+
+        }
+    }
+}
